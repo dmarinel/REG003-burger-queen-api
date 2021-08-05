9060bb9cc9e92fab7227841c3c8c7a7e6e52072f
Yessenia <vela.kathy@gmail.com>
2021-08-05 12:37:56 -0500

    crea model mongoose de user

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+const userSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true },
+    password: { type: String, select: false },
+    roles: {
+        admin: { type: Boolean }
+    }
+})
+
+userSchema.pre('save', (next) => {
+    let user = this
+    if (!user.isModified('password')) return next()
+
+    bcrypt.genSalt(10, (err,salt) =>{
+        if (err) return next(err)
+    
+        bcrypt.hash(user.password, salt, null, (err,hash) =>{
+            if (err) return next(err)
+            user.password = hash
+            next()
+        })
+    })
+})
+
+module.exports = mongoose.model('User', userSchema)
