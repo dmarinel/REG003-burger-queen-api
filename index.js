216d3cd31de899f267d8ee08bba402921446d67a
Yessenia <vela.kathy@gmail.com>
2021-08-04 19:51:45 -0500

    instalaciones y configuraciones del entorno

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
-
 const { port, dbUrl, secret } = config;
 const app = express();
 
 // TODO: Conexión a la Base de Datos (MongoDB o MySQL)
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db)=>console.info(db.connections))
+  .catch((err) => console.info(err);
 
 app.set('config', config);
 app.set('pkg', pkg);
