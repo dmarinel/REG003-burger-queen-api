ceadd7d0d86848d6fffecdbd6bc002b5ee795f4f
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-08-17 10:39:02 -0500

    update: create update

--- controller/auth.js
+++ controller/auth.js
@@ -12,39 +12,38 @@ const signIn = async (req, resp, next) => {
   }
 
   // TODO: autenticar a la usuarix
-  const userEmail = await User.findOne({
-    email,
-  });
+
   try {
-    // const userEmail = await User.findOne({
-    //   email,
-    // });
+    const userEmail = await User.findOne({
+      email,
+    });
 
     if (!userEmail) {
       return resp.status(400).json({
         message: 'This user does not exist!',
       });
     }
+
     const validPassword = await bcrypt.compare(password, userEmail.password);
     if (!validPassword) return resp.status(400).json({ message: 'Invalid Email or Password.' });
+
+    // Create a new token with email in the payload
+    jwt.sign({
+      // eslint-disable-next-line no-underscore-dangle
+      uid: userEmail._id,
+      email: userEmail.email,
+      roles: userEmail.roles,
+    }, secret, {
+      algorithm: 'HS256',
+      expiresIn: 300,
+    }, (err, token) => {
+      if (err) console.error(err);
+      return resp.status(200).json({ token });
+    });
   } catch (error) {
     console.log('line35');
     console.log(error);
   }
-
-  // Create a new token with email in the payload
-  jwt.sign({
-    // eslint-disable-next-line no-underscore-dangle
-    uid: userEmail._id,
-    email: userEmail.email,
-    roles: userEmail.roles,
-  }, secret, {
-    algorithm: 'HS256',
-    expiresIn: 300,
-  }, (err, token) => {
-    if (err) console.error(err);
-    return resp.status(200).json({ token });
-  });
 };
 
 module.exports = {
