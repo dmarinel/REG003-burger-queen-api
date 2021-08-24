e1e006161f2ff2c237403dbf4104640500fb4319
Yessenia Vela <vela.kathy@gmail.com>
2021-08-24 12:56:05 -0500

    trae cambios

--- models/user.js
+++ models/user.js
@@ -20,60 +20,16 @@ const userSchema = new Schema({
     admin: { type: Boolean },
   },
 });
-//------- Chio
-// userSchema.pre('save', function (next) {
-//   const user = this;
-//   if (!user.isModified('password')) return next();
 
-<<<<<<< HEAD
-//   bcrypt.hash(user.password, 10, (err, hash) => {
-//     if (err) return next(err);
-//     user.password = hash;
-//     next();
-//   });
-// });
-
-// // Encriptar contraseña cuando sea actualizado
-// userSchema.pre('findOneAndUpdate', function (next) {
-//   const user = this;
-//   if (!user._update.$set.password) return next();
-//   bcrypt.hash(user._update.$set.password, 10, (err, passwordHash) => {
-//     if (err) return next(err);
-//     user._update.$set.password = passwordHash;
-//     next();
-//   });
-// });
-//------------------------
-// fx que pre salva en la bd el objeto
-// hashea o encripta la contraseña antes de guardarla
-// userSchema.pre('save', function (next) {
-//   const user = this;
-//   if (!user.isModified('password')) return next();
-//   bcrypt.genSalt(10, (err, salt) => {
-//     if (err) return next(err);
-//     bcrypt.hash(user.password, salt, null, (err, hash) => {
-//       if (err) return next(err);
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
-
-// el método de encriptación traído de un video
-userSchema.methods.encryptPassword = async (password) => {
+userSchema.statics.encryptPassword = async (password) => {
   // cuantas veces quiero aplicar el algoritmo: 10 veces
   // termina de aplicar el método y me devuelve un salt 
-=======
-userSchema.statics.encryptPassword = async (password) => {
->>>>>>> 5b638bc70ea216cb1b786cd3847984497e83b40a
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
 
 // eslint-disable-next-line max-len
 userSchema.statics.comparePassword = async (password, receivedPassword) => await bcrypt.compare(password, receivedPassword);
-
-<<<<<<< HEAD
 
 // elimina la key password del objeto que retorna al momento de crear un usuario
 userSchema.methods.toJSON = function() {
@@ -88,9 +44,4 @@ userSchema.plugin(uniqueValidator, {
   message: '{PATH} debe de ser único'
 })
 
-
-
-module.exports = model('User', userSchema);
-=======
 module.exports = mongoose.model('User', userSchema);
->>>>>>> 5b638bc70ea216cb1b786cd3847984497e83b40a
