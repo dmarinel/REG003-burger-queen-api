1ce0735286d6dc349582d47cb9b3f1c799ddbeda
Yessenia Vela <vela.kathy@gmail.com>
2021-08-24 11:57:28 -0500

    borra líneas comentadas innecesarias

--- controller/users.js
+++ controller/users.js
@@ -1,44 +1,43 @@
+const config = require('../config');
+const User = require('../models/user');
+
 const getUsers = (req, resp, next) => {
   resp.send('get all users');
   // next();
 };
 
 const createUsers = (req, resp, next) => {
+  
+  const body = req.body;
+  if (!email || !password) {
+    return next(400);
+  }
+  console.log('user controller linea 15');
+  let usuario = new Usuario({
+    email,
+    password: bcrypt.hashSync(password, 10),
+    roles
+  });
+  usuario.save((err, usuarioDB) => {
+    console.log('aquí 19');
+    if (err) {
+      return res.status(400).json({
+         ok: false,
+         err,
+      });
+    }
+    res.json({
+          ok: true,
+          usuario: usuarioDB
+       });
+    })
+
   resp.send('create new users');
   next();
 };
+
 
 module.exports = {
   getUsers,
   createUsers,
 };
-
-// const User = require('../models/user');
-
-// const getUsers = (req, resp, next) => {
-//   resp.send('holi');
-//   resp.status(404).send('Sorry cant find that!');
-// };
-
-// // post
-// const createUser = (req, resp, next) => {
-//   console.log('POST USER');
-//   console.log(req.body);
-
-//   const product = new Product();
-//   product.name = req.body.name;
-//   product.price = req.body.price;
-//   product.image = req.body.image;
-//   product.type = req.body.type;
-//   product.dateEntry = req.body.type;
-
-//   product.save((err, productStored) => { // cuando se almacene, mongodb le adiciona un id
-// if (err) resp.status(500).send({ message: `Oppss.There is an error in the data base: ${err}` });
-//     else { res.status(200).send({ product: productStored }); }
-//   });
-// };
-
-// module.exports = {
-//   getUsers,
-//   createUser,
-// };
