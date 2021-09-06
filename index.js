f3fe0d3262c770ebd074fb3ce46c0e9574bc0a7e
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-09-06 10:28:36 -0500

    add: configure cors

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(authMiddleware(secret));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-COntrol-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Allow', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 // Registrar rutas
 routes(app, (err) => {
   if (err) {
